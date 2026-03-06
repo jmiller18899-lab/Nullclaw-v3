92c34242e98c9e06035ad44f80f3d9910d84f14c
Justin <jmiller18899@gmail.com>
2026-03-06 12:18:49 -0500

    Create main React component

---
+++ src/components/NullclawMissionControl.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NullclawMissionControl = () => {
+    return (
+        <div>
+            <h1>Nullclaw Mission Control</h1>
+        </div>
+    );
+};
+
+export default NullclawMissionControl;
